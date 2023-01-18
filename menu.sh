a6b076f2a9b17d935f3a37c69262d7b55bbf7e8b
ilizor <diegoseoges@gmail.com>
2023-01-18 16:21:53 +0100

    primer cambio na casa

--- menu.sh
+++ menu.sh
@@ -4,45 +4,54 @@ echo `clear`
 echo "Welcome"
 sair=true;
 echo "1.English 2.Spanish 3.Galician"
-read -p "Select a language" language
+read -p "Select a language (Type the number): " language
 
-#functions
-function menugl(){    
-    echo "## Menu ##"
-    echo "1.|Calculadora|"
-    echo "2.|Usuario actual|"
-    echo "3.|Linguaxe|"
-    echo "4.||"
-    echo "5.|Sair|"
-    echo "____________"
+### FUNCTIONS ###
+function menugl(){ 
+    echo "█████████████████████████"   
+    echo "█######### Menu ########█"
+    echo "█████████████████████████"
+    echo "█ 1.   |Calculadora|    █"
+    echo "█ 2.|Directorio Actual| █"
+    echo "█ 3.      |Data|        █"
+    echo "█ 4.        ||          █"
+    echo "█ 5.      |Sair|        █"
+    echo "█████████████████████████"
+    echo "_________________________"
     echo ""
     
     read -p "Escribe o numero que queras elexir: " num
     return $num
     
 }
-function menuen(){    
-    echo "## Menu ##"
-    echo "1.|Calculator|"
-    echo "2.|Actual User|"
-    echo "3.|Language|"
-    echo "4.||"
-    echo "5.|Exit|"
-    echo "____________"
+function menuen(){
+    echo "█████████████████████████"   
+    echo "█######### Menu ########█"
+    echo "█████████████████████████"
+    echo "█ 1.    |Calculator|    █"
+    echo "█ 2.|Current Directory| █"
+    echo "█ 3.      |Date|        █"
+    echo "█ 4.        ||          █"
+    echo "█ 5.      |Exit|        █"
+    echo "█████████████████████████" 
+    echo "_________________________"
     echo ""
     
     read -p "Type the option number: " num
     return $num
     
 }
-function menues(){    
-    echo "## Menu ##"
-    echo "1.|Calculadora|"
-    echo "2.|Usuario Actual|"
-    echo "3.|Lenguaje|"
-    echo "4.||"
-    echo "5.|Salir|"
-    echo "____________"
+function menues(){   
+    echo "█████████████████████████"  
+    echo "█######### Menu ########█"
+    echo "█████████████████████████"
+    echo "█ 1.   |Calculadora|    █"
+    echo "█ 2.|Directorio Actual| █"
+    echo "█ 3.      |Fecha|       █"
+    echo "█ 4.         ||         █"
+    echo "█ 5.      |Salir|       █"
+    echo "█████████████████████████" 
+    echo "_________________________"
     echo ""
     
     read -p "Type the option number: " num
@@ -55,27 +64,41 @@ function opcions(){
             Calculadora
         ;;
         2)
-            echo "Usuario actual: " `who`
+            echo `clear`
+            echo ""
+            echo "___________________________________"
+            echo ""
+            echo "Directorio Actual: "
+            echo `pwd`   
+            echo "___________________________________"
+            echo ""
+            sleep 1
+            echo "Volvendo ao menú..."
+            sleep 2
+            echo `clear`       
         ;;
         3)
-
+            echo "__________________________________"
+            echo ""
+            echo `date`
+            echo "__________________________________"
         ;;
         4)
             echo "aaa"
         ;;
         5)
-        echo ""
-        echo "Ata pronto!"
-        sleep 1
-        echo `clear`
-        sair=false;
+            echo ""
+            echo "Ata pronto!"
+            sleep 1
+            echo `clear`
+            sair=false;
         ;;        
         *)
-        echo ""
-        echo "Ata pronto!"
-        sleep 1
-        echo `clear`
-        sair=false;
+            echo ""
+            echo "Ata pronto!"
+            sleep 1
+            echo `clear`
+            sair=false;
         ;;
     esac
 }
@@ -85,27 +108,43 @@ function options(){
             Calculator
         ;;
         2)
-            echo "Actual User: " `who`
+            echo `clear`
+            echo ""
+            echo "___________________________________"
+            echo ""
+            echo "Current Directory: "
+            echo `pwd`
+            echo "___________________________________"
+            echo ""
+            sleep 1
+            echo "Going back to the menu..."
+            sleep 2
+            echo `clear`  
         ;;
         3)
-
+            echo "__________________________________"
+            echo ""
+            echo `date`
+            echo "__________________________________"
         ;;
         4)
             echo "aaa"
         ;;
         5)
-        echo ""
-        echo "See you soon!"
-        sleep 1
-        echo `clear`
-        sair=false;
+            echo ""
+            echo "See you soon!"
+            sleep 1
+            echo `clear`
+            sleep 1
+            echo `clear`
+            sair=false;
         ;;
         *)
-        echo ""
-        echo "See you soon!"
-        sleep 1
-        echo `clear`
-        sair=false;
+            echo ""
+            echo "See you soon!"
+            sleep 1
+            echo `clear`
+            sair=false;
         ;;
     esac
 }
@@ -115,27 +154,41 @@ function opciones(){
             Calculadoraes
         ;;
         2)
-            echo "Usuario actual: " `who`
+            echo `clear`
+            echo ""
+            echo "___________________________________"
+            echo ""
+            echo "Directorio Actual: "
+            echo `pwd`
+            echo "___________________________________"
+            echo ""
+            sleep 1
+            echo "Volviendo al menú..."
+            sleep 2
+            echo `clear`  
         ;;
         3)
-
+            echo "__________________________________"
+            echo ""
+            echo `date`
+            echo "__________________________________"        
         ;;
         4)
             echo "aaa"
         ;;
         5)
-        echo ""
-        echo "Hasta pronto!"
-        sleep 1
-        echo `clear`
-        sair=false;
+            echo ""
+            echo "Hasta pronto!"
+            sleep 1
+            echo `clear`
+            sair=false;
         ;;
         *)
-        echo ""
-        echo "Hasta pronto!"
-        sleep 1
-        echo `clear`
-        sair=false;
+            echo ""
+            echo "Hasta pronto!"
+            sleep 1
+            echo `clear`
+            sair=false;
         ;;
     esac
 }
@@ -146,17 +199,39 @@ function Calculadora(){
     read -p "Que queres facer? " numcalc
     case $numcalc in
         1)
+        echo `clear`
+        echo "          » SUMA"
+        echo ""
         read -p "Introduce o primeiro numero: " a
         read -p "Introduce o segundo numero: " b
-        echo "$a + $b = " $(($a + $b))
+        echo ""
+        echo "_________________________"
+        echo ""
+        echo "$a + $b = " $(($a + $b))        
+        echo "_________________________"
+        echo ""        
         ;;
         2)
+        echo `clear`
+        echo "          » RESTA"
+        echo ""
         read -p "Introduce o primeiro numero: " a
         read -p "Introduce o segundo numero: " b
+        echo ""
+        echo "_________________________"
+        echo ""
         echo "$a - $b = " $(($a - $b))
+        echo "_________________________"
+        echo ""
         ;;
         *)
-        echo "OPTION NOT FOUND"
+        echo `clear`
+        echo ""
+        echo "_________________________"
+        echo ""
+        echo "OPTION NOT FOUND"        
+        echo "_________________________"
+        echo ""
         ;;
     esac
     sleep 1
@@ -168,17 +243,39 @@ function Calculadoraes(){
     read -p "¿Que quieres hacer? " numcalc
     case $numcalc in
         1)
+        echo `clear`
+        echo "          » SUMA"
+        echo ""
         read -p "Introduce el primer numero: " a
         read -p "Introduce el segundo numero: " b
-        echo "$a + $b = " $(($a + $b))
+        echo ""
+        echo "_________________________"
+        echo ""
+        echo "$a + $b = " $(($a + $b))        
+        echo "_________________________"
+        echo ""
         ;;
         2)
+        echo `clear`
+        echo "          » RESTA"
+        echo ""
         read -p "Introduce el primer numero: " a
         read -p "Introduce el segundo numero: " b
-        echo "$a - $b = " $(($a - $b))
+        echo ""
+        echo "_________________________"
+        echo ""
+        echo "$a - $b = " $(($a - $b))        
+        echo "_________________________"
+        echo ""
         ;;
         *)
-        echo "OPTION NOT FOUND"
+        echo `clear`
+        echo ""
+        echo "_________________________"
+        echo ""
+        echo "OPTION NOT FOUND"        
+        echo "_________________________"
+        echo ""
         ;;
     esac
     sleep 1
@@ -190,23 +287,47 @@ function Calculator(){
     read -p "What do you want to do? " numcalc
     case $numcalc in
         1)
+        echo `clear`
+        echo "        » ADDITION"
+        echo ""
         read -p "Type the first number: " a
         read -p "Type the second number: " b
-        echo "$a + $b = " $(($a + $b))
+        echo ""
+        echo "_________________________"
+        echo ""
+        echo "$a + $b = " $(($a + $b))        
+        echo "_________________________"
+        echo ""
         ;;
         2)
+        echo `clear`
+        echo "       » ABSTRACTION"
+        echo ""
         read -p "Type the first number: " a
         read -p "Type the second number: " b
+        echo ""
+        echo "_________________________"
+        echo ""
         echo "$a - $b = " $(($a - $b))
+        echo "_________________________"
+        echo ""
         ;;
         *)
-        echo "OPTION NOT FOUND"
+        echo `clear`
+        echo ""
+        echo "_________________________"
+        echo ""
+        echo "OPTION NOT FOUND"        
+        echo "_________________________"
+        echo ""
         ;;
     esac
     sleep 1
 }
 
 
+
+### PROGRAM ###
 
 case $language in
     1)
