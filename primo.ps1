be27938402d2a6c7759270dd2bce02864832f3ed
Diego <a22diegosg@iessanclemente.net>
2023-01-26 14:35:18 +0100

    subidos exercicios powershell

---
+++ primo.ps1
@@ -0,0 +1,37 @@
+﻿#Incio do bucle while
+ do
+ {
+    #Declaración de variables
+    ###
+
+    $primo = "É primo"
+    $noprimo = "Non é primo"
+    $c=0;
+
+    
+    $num = Read-Host "Introduce un numero: "
+    for($i = 1; $i -le $num; $i++)             #Inicio bucle for
+    {
+        if(($num % $i) -eq 0)
+        {
+            $c++
+        }
+        
+    }   #Peche do bucle for
+
+
+        #Condicións para ser primo
+
+        if(($c -le 2) -and ($c -ne 1)-and ($c -ne 0)-and ($c -ne ""))
+        {
+            $primo
+        }        
+        
+        else
+        {
+            $noprimo
+        }
+   
+}
+while($num -ne 0) #Peche do bucle while
+exit 0
---
+++ xogoadivinanza.ps1
@@ -0,0 +1,23 @@
+﻿$min = Read-Host "Introduce o número máis pequeno"
+$max = Read-Host "Introduce o número máis grande"
+$random = Get-Random -Minimum $min -Maximum $max
+$contador=0
+
+$random
+
+do{
+    $num= Read-Host "Acerta o numero"
+    $contador++
+    if($num -lt $random)
+    {
+        echo "O número é maior"
+    }
+    elseif($num -gt $random)
+    {
+        echo "O número e maior"
+    }
+    else{
+        echo "Número correcto, noraboa."
+    }
+}
+while($num -ne $random)
